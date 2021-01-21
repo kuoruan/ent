666a44c8bf5a3ca1e4b28049adfe0dddc101664d
Xingwang Liao <kuoruan@gmail.com>
2021-01-21 14:14:17 +0000

    chore: init test import

---
+++ entc.go
@@ -0,0 +1,26 @@
+// +build ignore
+
+package main
+
+import (
+	"log"
+
+	"github.com/facebook/ent/entc"
+	"github.com/facebook/ent/entc/gen"
+)
+
+func main() {
+	opts := []entc.Option{
+		entc.TemplateFiles("./ent/template/debug.tmpl"),
+	}
+	err := entc.Generate("./ent/schema", &gen.Config{
+		Templates: []*gen.Template{
+			gen.MustParse(gen.NewTemplate("test_import").
+				ParseFiles("./ent/template/test_import.tmpl")),
+		},
+        Target: "./ent",
+	}, opts...)
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kuoruan/ent
+
+go 1.15
+
+require github.com/facebook/ent v0.5.4
